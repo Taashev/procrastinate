0a20de0fd3b5c37c66e87704baee71ac1c3b59cd
Arsen Taashev <taashev.a.r92@yandex.ru>
2022-01-11 16:32:48 +0300

    Добавил js
    * При нажатии на кнопку плавно меняется текст и картинка

---
+++ js/script.js
@@ -0,0 +1,36 @@
+let phrases = [
+  { text: 'отправить другу смешную гифку', image: 'https://code.s3.yandex.net/web-code/procrastinate/1.gif' },
+  { text: 'посмотреть скидки на авиабилеты', image: 'https://code.s3.yandex.net/web-code/procrastinate/2.png' },
+  { text: 'разобраться, о чём поют рэперы', image: 'https://code.s3.yandex.net/web-code/procrastinate/3.png' },
+  { text: 'Юрий Дудь', image: 'https://code.s3.yandex.net/web-code/procrastinate/4.png' },
+  { text: 'расставить книги на полке по цвету', image: 'https://code.s3.yandex.net/web-code/procrastinate/5.png' },
+  { text: 'читать про зарплаты в Сан-Франциско', image: 'https://code.s3.yandex.net/web-code/procrastinate/6.png' },
+  { text: 'прочитать новости и ужаснуться в комментариях', image: 'https://code.s3.yandex.net/web-code/procrastinate/7.png' },
+  { text: 'попасть в поток грустных песен и вспомнить все ошибки молодости', image: 'https://code.s3.yandex.net/web-code/procrastinate/8.png' },
+  { text: 'посмотреть трейлер сериала и заодно первый сезон', image: 'https://code.s3.yandex.net/web-code/procrastinate/9.png' },
+  { text: 'проверить непрочитанное в Telegram-каналах', image: 'https://code.s3.yandex.net/web-code/procrastinate/10.png' }
+];
+
+let btn = document.querySelector('.procrastinate__btn');
+let image = document.querySelector('.procrastinate__img');
+let text = document.querySelector('.procrastinate__text');
+let randomText = document.querySelector('.procrastinate__random-text');
+
+
+function getRandomElement(arr) {
+  let randomIndex = Math.floor(Math.random() * arr.length);
+  return arr[randomIndex];
+}
+
+btn.addEventListener('click', function() {
+  let randomElement = getRandomElement(phrases);
+  smoothly(randomText, 'textContent', randomElement.text);
+  smoothly(image, 'src', randomElement.image);
+
+  if (text.textContent.length > 40) {
+    text.style.fontSize = '33px';
+  }
+  else {
+    text.style.fontSize = '40px';
+  }
+});
